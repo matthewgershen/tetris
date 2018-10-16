772a6ed6f588a6d96426c445030126e893caebd0
Matthew Gershen <matthewgershen@gmail.com>
2018-10-16 16:00:23 -0400

    Add floor collision

--- game.js
+++ game.js
@@ -5,8 +5,10 @@ class Game{
   constructor(canvas,ctx){
     this.ctx = ctx;
     this.canvas = canvas;
-    this.activePiece = []
+    this.activePiece = [];
     this.blocks = [];
+    this.collisionHandling = this.collisionHandling.bind(this);
+    this.collisionCheck = this.collisionCheck.bind(this);
   }
 
 
@@ -17,6 +19,7 @@ class Game{
       const blocks = piece.addPiece(this.nextType());
       this.activePiece = blocks;
     }
+    this.collisionHandling();
     this.ctx.clearRect(0,0,this.canvas.width, this.canvas.height);
     this.activePiece.forEach((block)=>{
       block.drawBlock(this.ctx);
@@ -26,6 +29,25 @@ class Game{
   nextType(){
     const types = ["i","o","t","s","z","j","l"]
     return types[Math.floor(Math.random() * types.length)];
+  }
+
+  collisionHandling(){
+    debugger
+    if (this.collisionCheck()) {
+      this.activePiece.forEach((block)=>{
+        block.dy = 0;
+      });
+    }
+  }
+
+  collisionCheck(){
+    var result = false
+    this.activePiece.forEach((block)=>{
+      if (block.y >= 760) {
+        result = true;
+      }
+    });
+    return result
   }
 }
 
