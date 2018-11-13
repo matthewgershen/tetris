8beb091abd1c255ef5fcaaab3be0d60008061f69
Matthew Gershen <matthewgershen@gmail.com>
2018-11-13 12:36:17 -0500

    Add music

--- tetris.js
+++ tetris.js
@@ -7,6 +7,19 @@ document.addEventListener("DOMContentLoaded", () => {
   let animationId = null;
   const restart = document.getElementById("restart");
 
+  const mute = document.getElementById("mute");
+  const unmute = document.getElementById("unmute");
+  mute.addEventListener("click", (e)=>{
+    document.getElementById('background_audio').muted = true;
+    document.getElementById('unmute').hidden = false;
+    document.getElementById('mute').hidden = true;
+  });
+  unmute.addEventListener("click", (e)=>{
+    document.getElementById('background_audio').muted = false;
+    document.getElementById('unmute').hidden = true;
+    document.getElementById('mute').hidden = false;
+  });
+
   document.addEventListener("keydown", game.keydownHandler);
   document.addEventListener("keyup", game.keyupHandler);
   game.drawSides();
